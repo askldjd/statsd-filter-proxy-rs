22b88ceab2de683ecf152255013781568773f275
Matthias Beyer <mail@beyermatthias.de>
2021-05-14 10:38:56 +0200

    Refactor: Only join string parts if there are any
    This refactoring might have a performance impact (cannot measure).
    With this patch, the return value in the `filter()` function is not a
    `String` anymore, but a list of references: `Vec<&str>`.
    
    Previously, the `String` was only processed further if it was non-empty.
    An empty Vec<&str> is the same as an empty `String`: Not relevant for
    further processing.
    
    Hence, we can optimize this and join the `Vec<&str>` to `String` only if
    it is not empty.
    
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/filter.rs
+++ src/filter.rs
@@ -1,6 +1,6 @@
 use std::str;
 
-pub fn filter(block_list: &Vec<String>, buf: &[u8]) -> String {
+pub fn filter<'a>(block_list: &'a Vec<String>, buf: &'a [u8]) -> Vec<&'a str> {
     let statsd_str = unsafe { str::from_utf8_unchecked(&buf) };
 
     statsd_str.split("\n")
@@ -8,7 +8,6 @@ pub fn filter(block_list: &Vec<String>, buf: &[u8]) -> String {
             !block_list.iter().any(|prefix| line.starts_with(prefix))
         })
         .collect::<Vec<&str>>()
-        .join("\n")
 }
 
 #[cfg(test)]
@@ -19,7 +18,7 @@ mod tests {
     fn test_should_not_block_multi_metric() {
         let block_list = vec![String::from("notfoo"), String::from("otherfoo")];
         let statsd_str_bytes = "foo:1|c\nfoo:2|c\nfoo:3|c".as_bytes();
-        let result = filter(&block_list, &statsd_str_bytes);
+        let result = filter(&block_list, &statsd_str_bytes).join("\n");
         assert_eq!("foo:1|c\nfoo:2|c\nfoo:3|c", result);
     }
 
@@ -28,7 +27,7 @@ mod tests {
     fn test_should_not_block_single_metric() {
         let block_list = vec![String::from("notfoo"), String::from("otherfoo")];
         let statsd_str_bytes = "foo:1|c".as_bytes();
-        let result = filter(&block_list, &statsd_str_bytes);
+        let result = filter(&block_list, &statsd_str_bytes).join("\n");
         assert_eq!("foo:1|c", result);
     }
 
@@ -36,7 +35,7 @@ mod tests {
     fn test_should_block_completely_single_metric() {
         let block_list = vec![String::from("foo"), String::from("otherfoo")];
         let statsd_str_bytes = "foo:1|c".as_bytes();
-        let result = filter(&block_list, &statsd_str_bytes);
+        let result = filter(&block_list, &statsd_str_bytes).join("\n");
         assert_eq!("", result);
     }
 
@@ -44,7 +43,7 @@ mod tests {
     fn test_should_block_completely_multi_metric() {
         let block_list = vec![String::from("foo"), String::from("otherfoo")];
         let statsd_str_bytes = "foo:1|c\nfoo:2|c\nfoo:3|c".as_bytes();
-        let result = filter(&block_list, &statsd_str_bytes);
+        let result = filter(&block_list, &statsd_str_bytes).join("\n");
         assert_eq!("", result);
     }
 
@@ -52,7 +51,7 @@ mod tests {
     fn test_should_block_partially_multi_metric() {
         let block_list = vec![String::from("foo"), String::from("otherfoo")];
         let statsd_str_bytes = "notfoo:1|c\nfoo:2|c\nnotfoo:3|c".as_bytes();
-        let result = filter(&block_list, &statsd_str_bytes);
+        let result = filter(&block_list, &statsd_str_bytes).join("\n");
         assert_eq!("notfoo:1|c\nnotfoo:3|c", result);
     }
 }
